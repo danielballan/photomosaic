a58630a911a48f60a39010c92a9be00bd0b737c3
David Lu <davidvlu@gmail.com>
2014-10-30 23:33:02 -0500

    Simpler directory walker

--- directory_walker.py
+++ directory_walker.py
@@ -1,26 +1,16 @@
 import os
 
 class DirectoryWalker:
-    """A forward iterator that traverses the files in a directory tree.
-    Source: http://effbot.org/librarybook/os-path-walk-example-3.py"""
     def __init__(self, directory):
-        self.stack = [directory]
         self.files = []
         self.index = 0
+        
+        for root,dirs,files in os.walk(directory):
+            for f in files:
+                self.files.append( root + os.sep + f )
+                
+    def __len__(self):
+        return len(self.files)
 
     def __getitem__(self, index):
-        while 1:
-            try:
-                filename = self.files[self.index]
-                self.index = self.index + 1
-            except IndexError:
-                # pop next directory from stack
-                self.directory = self.stack.pop()
-                self.files = os.listdir(self.directory)
-                self.index = 0
-            else:
-                # got a filename
-                fullname = os.path.join(self.directory, filename)
-                if os.path.isdir(fullname) and not os.path.islink(fullname):
-                    self.stack.append(fullname)
-                return fullname
+        return self.files[index]
