7f1ddec9e170941e3a5159236ede817c2d569f38
danielballan <daniel.b.allan@gmail.com>
2016-10-08 15:59:37 -0400

    TST: Update test to match API.

--- graphical_tests/test_partition.py
+++ graphical_tests/test_partition.py
@@ -4,6 +4,8 @@ Use the same array as both the image and the mask.
 The tiles should subdivide along the curved edge to trace out a smooth circle.
 """
 from skimage import draw
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import photomosaic as pm
@@ -12,5 +14,5 @@ img = np.zeros((1000, 1000))
 rr, cc = draw.circle(300, 500, 150)
 img[rr, cc] = 1
 tiles = pm.partition(img, (10, 10), mask=img.astype(bool), depth=3)
-plt.imshow(pm.draw_tiles(img, tiles, color=0.5))
+plt.imshow(pm.draw_tile_layout(img, tiles, color=0.5))
 plt.savefig('test-partition.png')
