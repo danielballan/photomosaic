2d09314ab58bb766372dc6e263fb17428b1fd3cd
danielballan <daniel.b.allan@gmail.com>
2016-11-17 14:59:55 -0500

    BLD: Fix check for existing pools.

--- doc/pool_scripts/cats.py
+++ doc/pool_scripts/cats.py
@@ -3,7 +3,7 @@ import photomosaic.flickr
 import photomosaic as pm
 
 
-if not os.path.isfile('~/pools/cats/pool.json'):
+if not os.path.isfile(os.path.expanduser('~/pools/cats/pool.json')):
     FLICKR_API_KEY = os.environ['FLICKR_API_KEY']
     pm.set_options(flickr_api_key=FLICKR_API_KEY)
 
