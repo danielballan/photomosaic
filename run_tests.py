0ff658fe7bd695c9587e1fe3612b935f77f1368e
Dan Allan <daniel.b.allan@gmail.com>
2019-06-21 11:41:58 -0400

    Remove run_tests.py. (Just execute pytest.)
    The `pytest.ini` provides the information pytest needs, so there is no
    need for a custom file.

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-import pytest
-
-if __name__ == '__main__':
-    args = ['-vrxs', '--cov', 'photomosaic', 'test.py']
-    if len(sys.argv) > 1:
-        args.extend(sys.argv[1:])
-    sys.exit(pytest.main(args))
