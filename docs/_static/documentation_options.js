2ca4702ddc5f5ebb9673d5a8d9650fb608bbe5d7
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-01 12:23:22 +0000

    Update docs after building Travis build 143 of danielballan/photomosaic
    The docs were built from the branch 'master' against the commit
    913c49a1c40e8af3b1704f3b13af6d5576d47575.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/danielballan/photomosaic/jobs/731910337.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.0/bin/doctr deploy docs --built-docs doc/build/html

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1+10.g7d65b3f',
+    VERSION: '0.3.1+11.g913c49a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
